9de3c242a0914b1e2dd6d5c5f2af2857973a4369
serhii-bohdanov <serega.214108@gmail.com>
2026-08-23 14:17:24 +0300

    fix bug

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,39 +1,42 @@
-const { getUser } = require('../test-data/users');
-const { _loginLocators } = require('../locators/_login.locators');
+const {getUser} = require('../test-data/users');
+const {_loginLocators} = require('../locators/_login.locators');
 
 class LoginPage {
-  #page;
-  #usernameInput;
-  #passwordInput;
-  #loginButton;
+    #page;
+    #usernameInput;
+    #passwordInput;
+    #loginButton;
 
-  constructor(page) {
-    this.#page = page;
+    constructor(page) {
+        this.#page = page;
 
-    this.#usernameInput = page.locator(_loginLocators.usernameInput);
-    this.#passwordInput = page.locator(_loginLocators.passwordInput);
-    this.#loginButton = page.locator(_loginLocators.loginButton);
-  }
+        this.#usernameInput = page.locator(_loginLocators.usernameInput);
+        this.#passwordInput = page.locator(_loginLocators.passwordInput);
+        this.#loginButton = page.locator(_loginLocators.loginButton);
+    }
 
-  async goto() {
-    await this.#page.goto(process.env.BASE_URL);
-  }
+    async goto() {
+        if (!process.env.BASE_URL) {
+            throw new Error('BASE_URL environment variable is not set');
+        }
+        await this.#page.goto(process.env.BASE_URL);
+    }
 
-  async fillCredentials({ username, password }) {
-    await this.#usernameInput.fill(username);
-    await this.#passwordInput.fill(password);
-  }
+    async fillCredentials({username, password}) {
+        await this.#usernameInput.fill(username);
+        await this.#passwordInput.fill(password);
+    }
 
-  async clickLoginButton() {
-    await this.#loginButton.click();
-  }
+    async clickLoginButton() {
+        await this.#loginButton.click();
+    }
 
-  async login(user) {
-    const userData = user || getUser('mainUser');
+    async login(user) {
+        const userData = user || getUser('mainUser');
 
-    await this.fillCredentials(userData);
-    await this.clickLoginButton();
-  }
+        await this.fillCredentials(userData);
+        await this.clickLoginButton();
+    }
 }
 
-module.exports = { LoginPage };
+module.exports = {LoginPage};
